9a3acac29b2e15c8769c1104753b97c22edd0940
lesterinfotech <lnidhan@infotechcaribbean.com>
2016-08-10 00:10:32 -0400

    Added Server/Daemon app client

---
+++ WebApp-B2C-DotNet/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp_B2C_DotNet.Models
+{
+    public class TodoItem
+    {
+        public string Title { get; set; }
+        public string Owner { get; set; }
+    }
+}
